8bc7c5fc566ca5acb012b7530bd9db88eccfdc16
Jungwoo-Son <jungwoo9356@gmail.com>
2020-08-27 19:31:29 +0900

    [ADD] bind apis

--- Jeolla/src/loaders/express.js
+++ Jeolla/src/loaders/express.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const apis = require('../apis');
+
 module.exports = (app) => {
     app.use(cors());
     app.use(express.json());
+
+
+    app.use(apis);
 };
 
 
