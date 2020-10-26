e01b857ffc2709dbf5bea7b30d6ba0ff7b21b1c6
Jungwoo-Son <jungwoo9356@gmail.com>
2020-10-26 23:02:54 +0900

    [FIX] some typo

--- Jeolla/src/spec/config.spec.js
+++ Jeolla/src/spec/config.spec.js
@@ -4,19 +4,19 @@ const config = require('../conigs');
 describe('Config test', () => {
     describe('dev environment', () => {
         it('SERVER_PORT', () => {
-            assert.strictEqual(process.env.SERVER_PORT, 8081);
+            assert.strictEqual(config.SERVER_PORT, 8081);
         });
         it('DB_PASSWORD', () => {
-            assert.strictEqual(process.env.DB_PASSWORD, '11111111');
+            assert.strictEqual(config.DB_PASSWORD, '11111111');
         });
         it('DB_DATABASE', () => {
-            assert.strictEqual(process.env.DB_DATABASE, 'dis');
+            assert.strictEqual(config.DB_DATABASE, 'dis');
         });
         it('DB_USERNAME', () => {
-            assert.strictEqual(process.env.DB_USERNAME, 'root');
+            assert.strictEqual(config.DB_USERNAME, 'root');
         });
         it('DB_HOST', () => {
-            assert.strictEqual(process.env.DB_HOST, 'localhost');
+            assert.strictEqual(config.DB_HOST, 'localhost');
         });
     });
 });
